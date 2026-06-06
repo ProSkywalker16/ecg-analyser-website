9edbac7150906d7f000d6e187f0f8c4c02384a0f
Promit Chaudhuri <134211908+ProSkywalker16@users.noreply.github.com>
2026-06-06 22:02:58 +0530

    resolve server crash

--- ecg-analyser-web/server/index.js
+++ ecg-analyser-web/server/index.js
@@ -125,7 +125,7 @@ app.use((err, req, res, next) => {
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
-  startKeepAlive();
+  // startKeepAlive();
 });
 /* I deactivated this function to prevent free tier runout but it actually works
 // ─── Keep-Alive Self-Ping ────────────────────────────────────────────────────
